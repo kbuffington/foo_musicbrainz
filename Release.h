8075e80f48536995b186ba93518165811c50f34d
Dremora <byrdmaniac@gmail.com>
2011-05-26 21:51:29 +0300

    Converted Release entity to NGS

--- Release.h
+++ Release.h
@@ -1,76 +1,29 @@
 #pragma once
 
-#include "Track.h"
-
-#define MB_RELEASE_TYPES 12
-#define MB_RELEASE_STATUSES 5
-
-class Release
-{
-public:
-	Release(const char *_title, const char *_id, const char *_artist, const char *_artist_id);
-	Track *addTrack(const char *_title, const char *_id);
-	const char *getArtist();
-	const char *getArtistId();
-	const char *getDate();
-	const char *getDisc();
-	const char *getDiscSubtitle();
-	const char *getId();
-	unsigned int getStatus();
-	const char *getStatusText();
-	const char *getTitle();
-	unsigned int getType();
-	const char *getTypeText();
-	Track *getTrack(unsigned int number);
-	unsigned int getTracksCount();
-	void setArtist(const char *_artist);
-	void setDate(const char *_date);
-	void setDisc(const char *_disc);
-	void setDiscSubtitle(const char *_discsubtitle);
-	void setStatus(unsigned int _status);
-	void setStatus(const char *_status);
-	void setTitle(const char *_title);
-	void setType(unsigned int _type);
-	void setType(const char *_type);
-	bool va;
-	~Release();
-
-	static const char *Release::Statuses[];
-	static const char *Release::Types[];
-
-private:
-	pfc::list_t <Track *> tracks;
-	pfc::string8 artist;
-	pfc::string8 artist_id;
-	pfc::string8 date;
-	pfc::string8 disc;
-	pfc::string8 discsubtitle;
-	pfc::string8 id;
-	unsigned int status;
-	pfc::string8 title;
-	unsigned int type;
-};
-
-
 #include "foo_musicbrainz.h"
 #include "CoreEntity.h"
 #include "ArtistCredit.h"
+#include "MediumList.h"
 #include "Date.h"
 
 namespace foo_musicbrainz {
 	class Release : public CoreEntity {
 		STRING_MEMBER(title)
-		MEMBER_BY_REFERENCE(Date, date)
 		STRING_MEMBER(barcode)
-		STRING_MEMBER(status)
 		STRING_MEMBER(country)
 		STRING_MEMBER(asin)
+		MEMBER_BY_REFERENCE(Date, date)
 		POINTER_MEMBER(ArtistCredit, artist_credit)
+		POINTER_MEMBER(MediumList, medium_list)
+		STRING_LIST(status, statuses, 4)
 
 	public:
-		Release() : artist_credit(nullptr) {};
+		Release() :
+			artist_credit(nullptr),
+			medium_list(nullptr) {};
 		~Release() {
 			POINTER_MEMBER_DESTRUCTOR(artist_credit)
+			POINTER_MEMBER_DESTRUCTOR(medium_list)
 		}
 	};
 }
--- meta.h
+++ meta.h
@@ -33,6 +33,12 @@
 	GETTER_BY_REFERENCE(type, name) \
 	SETTER_BY_REFERENCE(type, name)
 
+#define MEMBER(type, name) \
+	protected: \
+		type name; \
+	public: \
+	ACCESSOR(type, name)
+
 #define MEMBER_BY_REFERENCE(type, name) \
 	protected: \
 		type name; \
@@ -53,11 +59,40 @@
 		} \
 		name = value; \
 	} \
-	const type &get_##name() const { \
-		return *this->name; \
+	type *get_##name() const { \
+		return this->name; \
 	}
 
 #define POINTER_MEMBER_DESTRUCTOR(name) \
 	if (name != nullptr) { \
 		delete name; \
 	}
+
+#define STRING_LIST(name, name_plural, count) \
+	public: \
+	void set_##name(size_t i) { \
+		if (i < count) { \
+			name = i; \
+		} else { \
+			name = 0; \
+		} \
+	} \
+	void set_##name(const char *new_##name) { \
+		for (size_t i = 0; i < count; i++) { \
+			if (_stricmp(new_##name, name_plural[i]) == 0) { \
+				name = i; \
+				return; \
+			} \
+		} \
+		name = 0; \
+	} \
+	pfc::string8 get_##name() { \
+		return pfc::string8(name_plural[name]); \
+	} \
+	size_t get_##name##_index() { \
+		return name; \
+	} \
+	private: \
+		static const char *name_plural[]; \
+		static const size_t name##_count = count; \
+		size_t name;
