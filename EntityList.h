dcd8ae601a62ccf8696919d8eaa80168b958d5f5
Dremora <byrdmaniac@gmail.com>
2011-05-27 00:10:21 +0300

    Merged EntityList definition and declaration into a single file

--- EntityList.h
+++ EntityList.h
@@ -6,12 +6,27 @@ namespace foo_musicbrainz {
 	template <class T>
 	class EntityList {
 	public:
-		void add(T *item);
-		size_t count() const;
-		T *operator[](t_size index) const ;
+		void add(T *item) {
+			items.add_item(item);
+		};
+
+		size_t count() const{
+			return items.get_count();
+		}
+
+		T *operator[](t_size index) const {
+			if (index >= items.get_count()) {
+				throw pfc::exception_overflow();
+			}
+			return items[index];
+		}
 
 		EntityList() {};
-		~EntityList() {};
+		~EntityList() {
+			for (unsigned int i = 0; i < items.get_count(); i++) {
+				delete items[i];
+			}
+		}
 
 	protected:
 		pfc::list_t<T *> items;
