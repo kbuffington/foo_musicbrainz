8e7cf3136a690669074ee19e7d5f41df9b6b2bed
Dremora <byrdmaniac@gmail.com>
2011-05-26 23:24:08 +0300

    Moved list_view_edit to foo_musicbrainz namespace

--- list_view_edit.cpp
+++ list_view_edit.cpp
@@ -1,50 +1,42 @@
 #include "foo_musicbrainz.h"
-#include "ReleaseList.h"
-#include "Track.h"
 #include "list_view_edit.h"
+#include "ReleaseList.h"
 
-void track_list_view_edit::Attach(HWND _window, ReleaseList *_mbc)
-{
-	window = _window;
-	mbc = _mbc;
+using namespace foo_musicbrainz;
+
+void track_list_view_edit::Attach(HWND window, ReleaseList *mbc) {
+	this->window = window;
+	this->mbc = mbc;
 }
 
-void track_list_view_edit::Start(t_size item, t_size subItem)
-{
+void track_list_view_edit::Start(t_size item, t_size subItem) {
 	TableEdit_Start(item, subItem);
 }
 
-void track_list_view_edit::Abort()
-{
+void track_list_view_edit::Abort() {
 	TableEdit_Abort(true);
 }
 
-bool track_list_view_edit::IsActive()
-{
+bool track_list_view_edit::IsActive() {
 	return TableEdit_IsActive();
 }
 
-HWND track_list_view_edit::TableEdit_GetParentWnd() const
-{
+HWND track_list_view_edit::TableEdit_GetParentWnd() const {
 	return window;
 }
 
-bool track_list_view_edit::TableEdit_IsColumnEditable(t_size subItem) const
-{
-	if (subItem == 0) return false;
-	else return true;
+bool track_list_view_edit::TableEdit_IsColumnEditable(t_size subItem) const {
+	return subItem != 0;
 }
 
-void track_list_view_edit::TableEdit_SetField(t_size item, t_size subItem, const char * value)
-{
-	switch (subItem)
-	{
-	case 1:
-		mbc->getRelease()->getTrack(item)->setTitle(value);
-		break;
-	case 2:
-		mbc->getRelease()->getTrack(item)->setArtist(value);
-		break;
+void track_list_view_edit::TableEdit_SetField(t_size item, t_size subItem, const char *value) {
+	switch (subItem) {
+	//case 1:
+	//	mbc->getRelease()->getTrack(item)->setTitle(value);
+	//	break;
+	//case 2:
+	//	mbc->getRelease()->getTrack(item)->setArtist(value);
+	//	break;
 	}
 	CTableEditHelperV2_ListView::TableEdit_SetField(item, subItem, value);
 }
--- list_view_edit.h
+++ list_view_edit.h
@@ -1,20 +1,23 @@
 #pragma once
 
-#include "Track.h"
+#include "foo_musicbrainz.h"
+#include "ReleaseList.h"
 
-class track_list_view_edit : public InPlaceEdit::CTableEditHelperV2_ListView {
-public:
-	void Attach(HWND _window, ReleaseList *_mbc);
-	void Start(t_size item, t_size subItem);
-	void Abort();
-	bool IsActive();
+namespace foo_musicbrainz {
+	class track_list_view_edit : public InPlaceEdit::CTableEditHelperV2_ListView {
+	public:
+		void Attach(HWND window, ReleaseList *mbc);
+		void Start(t_size item, t_size subItem);
+		void Abort();
+		bool IsActive();
 
-protected:
-	HWND TableEdit_GetParentWnd() const;
-	bool TableEdit_IsColumnEditable(t_size subItem) const;
-	void TableEdit_SetField(t_size item, t_size subItem, const char * value);
+	protected:
+		HWND TableEdit_GetParentWnd() const;
+		bool TableEdit_IsColumnEditable(t_size subItem) const;
+		void TableEdit_SetField(t_size item, t_size subItem, const char *value);
 
-private:
-	ReleaseList *mbc;
-	HWND window;
-};
+	private:
+		ReleaseList *mbc;
+		HWND window;
+	};
+}
