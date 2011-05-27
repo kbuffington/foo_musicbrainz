c83e6f96f3498983a161236e39cef368ed32f98e
Dremora <byrdmaniac@gmail.com>
2011-05-27 07:19:15 +0300

    Oops, adding forgotten header with preferences declarations

---
+++ Preferences.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "foo_musicbrainz.h"
+
+namespace foo_musicbrainz {
+	namespace Preferences {
+		extern const GUID guid_short_date;
+		extern cfg_bool short_date;
+
+		extern const GUID guid_no_feat;
+		extern cfg_bool no_feat;
+
+		extern const GUID guid_write_ids;
+		extern cfg_bool write_ids;
+
+		extern const GUID guid_albumtype;
+		extern cfg_bool albumtype;
+
+		extern const GUID guid_albumstatus;
+		extern cfg_bool albumstatus;
+
+		extern const GUID guid_albumtype_data;
+		extern cfg_string albumtype_data;
+
+		extern const GUID guid_albumstatus_data;
+		extern cfg_string albumstatus_data;
+	}
+}
