1165bc5fd277b6b2b25bb3a842d973591e907dc1
marc2k3 <34617027+marc2k3@users.noreply.github.com>
2016-05-20 03:22:21 +0100

    Last update by Dremora was in 2012

--- foo_musicbrainz/foo_musicbrainz.cpp
+++ foo_musicbrainz/foo_musicbrainz.cpp
@@ -4,5 +4,5 @@ DECLARE_COMPONENT_VERSION(
 	COMPONENT_TITLE,
 	COMPONENT_VERSION,
 	"Build: "  __TIME__ ", " __DATE__ "\n"
-	"Copyright (C) 2009-2011 Dremora\n"
+	"Copyright (C) 2009-2012 Dremora"
 );
