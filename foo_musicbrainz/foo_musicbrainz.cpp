254167673c350ab0ddf7765ad48551401d2fa950
marc2k3 <34617027+marc2k3@users.noreply.github.com>
2016-08-27 20:46:55 +0100

    Add myself to about dialog

--- foo_musicbrainz/foo_musicbrainz.cpp
+++ foo_musicbrainz/foo_musicbrainz.cpp
@@ -4,5 +4,6 @@ DECLARE_COMPONENT_VERSION(
 	COMPONENT_TITLE,
 	COMPONENT_VERSION,
 	"Build: "  __TIME__ ", " __DATE__ "\n"
-	"Copyright (C) 2009-2012 Dremora"
+	"Copyright (C) 2009-2012 Dremora\n"
+	"Copyright (C) 2015-2016 marc2003"
 );
