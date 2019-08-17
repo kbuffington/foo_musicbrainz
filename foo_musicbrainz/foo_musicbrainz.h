ba2b1d63fde045e16c26557c2cb0a7e39ae940c7
marc2k3 <34617027+marc2k3@users.noreply.github.com>
2019-08-17 05:08:55 +0100

    Replace SDK with submodule

--- foo_musicbrainz/foo_musicbrainz.h
+++ foo_musicbrainz/foo_musicbrainz.h
@@ -7,9 +7,10 @@
 #define COMPONENT_VERSION "0.3.10"
 
 #include "resource.h"
-#include "../foobar2000/ATLHelpers/ATLHelpersLean.h"
-#include "../foobar2000/ATLHelpers/misc.h"
-#include "../foobar2000/ATLHelpers/inplace_edit_v2.h"
+#include "../foobar2000-SDK/foobar2000/helpers/foobar2000+atl.h"
+#include "../foobar2000-SDK/foobar2000/helpers/atl-misc.h"
+#include "../foobar2000-SDK/libPPUI/InPlaceEditTable.h"
+#include "../foobar2000-SDK/libPPUI/listview_helper.h"
 #include "../tinyxml/tinyxml.h"
 #include "meta.h"
 #include "Preferences.h"
