9a7ec38af251a1878a87f5b2fea4f40ad8d8d00f
marc2k3 <34617027+marc2k3@users.noreply.github.com>
2019-09-16 07:25:01 +0100

    Tidy up SDK includes

--- src/stdafx.h
+++ src/stdafx.h
@@ -13,10 +13,10 @@ using std::regex;
 using json = nlohmann::json;
 
 #include "resource.h"
-#include "../foobar2000-SDK/foobar2000/helpers/foobar2000+atl.h"
-#include "../foobar2000-SDK/foobar2000/helpers/atl-misc.h"
-#include "../foobar2000-SDK/libPPUI/CListControlOwnerData.h"
-#include "../foobar2000-SDK/libPPUI/listview_helper.h"
+#include <foobar2000/helpers/foobar2000+atl.h>
+#include <foobar2000/helpers/atl-misc.h>
+#include <libPPUI/CListControlOwnerData.h>
+#include <libPPUI/listview_helper.h>
 
 using namespace pfc::stringcvt;
 using str8 = pfc::string8;
