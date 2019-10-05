decd6774934eb8a265b58eafd809dea6f97b3aab
marc2k3 <34617027+marc2k3@users.noreply.github.com>
2019-10-05 13:48:10 +0100

    Tidy up preferences

--- src/mb_preferences.cpp
+++ src/mb_preferences.cpp
@@ -2,61 +2,61 @@
 
 namespace mb_preferences
 {
-	const GUID guid_server = { 0x2ac00b3b, 0x1b04, 0x4fb2,{ 0xa9, 0x98, 0x5c, 0x16, 0x4, 0x9c, 0xce, 0x9d } };
-	const bool default_server = false;
+	constexpr GUID guid_server = { 0x2ac00b3b, 0x1b04, 0x4fb2,{ 0xa9, 0x98, 0x5c, 0x16, 0x4, 0x9c, 0xce, 0x9d } };
+	constexpr bool default_server = false;
 	cfg_bool server(guid_server, default_server);
 
-	const GUID guid_short_date = { 0x18734618, 0x7920, 0x4d24,{ 0x84, 0xa1, 0xb9, 0xd6, 0x6e, 0xd8, 0x25, 0xbc } };
-	const bool default_short_date = false;
+	constexpr GUID guid_short_date = { 0x18734618, 0x7920, 0x4d24,{ 0x84, 0xa1, 0xb9, 0xd6, 0x6e, 0xd8, 0x25, 0xbc } };
+	constexpr bool default_short_date = false;
 	cfg_bool short_date(guid_short_date, default_short_date);
 
-	const GUID guid_ascii_punctuation = { 0xd08b1b7c, 0x38fd, 0x4689,{ 0x9e, 0x91, 0x8c, 0xdc, 0xbe, 0xc4, 0x26, 0x98 } };
-	const bool default_ascii_punctuation = false;
+	constexpr GUID guid_ascii_punctuation = { 0xd08b1b7c, 0x38fd, 0x4689,{ 0x9e, 0x91, 0x8c, 0xdc, 0xbe, 0xc4, 0x26, 0x98 } };
+	constexpr bool default_ascii_punctuation = false;
 	cfg_bool ascii_punctuation(guid_ascii_punctuation, default_ascii_punctuation);
 
-	const GUID guid_write_ids = { 0x8c8b61e0, 0x8eea, 0x4c34,{ 0x9a, 0x51, 0x4d, 0xa9, 0x9c, 0xec, 0xcb, 0xbc } };
-	const bool default_write_ids = true;
+	constexpr GUID guid_write_ids = { 0x8c8b61e0, 0x8eea, 0x4c34,{ 0x9a, 0x51, 0x4d, 0xa9, 0x9c, 0xec, 0xcb, 0xbc } };
+	constexpr bool default_write_ids = true;
 	cfg_bool write_ids(guid_write_ids, default_write_ids);
 
-	const GUID guid_albumtype = { 0xd8fdb1d8, 0xde2, 0x4f1f,{ 0x85, 0x5a, 0xc0, 0x5, 0x98, 0x60, 0x9b, 0xe9 } };
+	constexpr GUID guid_albumtype = { 0xd8fdb1d8, 0xde2, 0x4f1f,{ 0x85, 0x5a, 0xc0, 0x5, 0x98, 0x60, 0x9b, 0xe9 } };
 	const bool default_albumtype = true;
 	cfg_bool albumtype(guid_albumtype, default_albumtype);
 
-	const GUID guid_albumstatus = { 0x57a8dddd, 0xdf24, 0x4fd3,{ 0xac, 0x95, 0x6f, 0x8, 0x3, 0x26, 0x41, 0x7c } };
-	const bool default_albumstatus = true;
+	constexpr GUID guid_albumstatus = { 0x57a8dddd, 0xdf24, 0x4fd3,{ 0xac, 0x95, 0x6f, 0x8, 0x3, 0x26, 0x41, 0x7c } };
+	constexpr bool default_albumstatus = true;
 	cfg_bool albumstatus(guid_albumstatus, default_albumstatus);
 
-	const GUID guid_write_label_info = { 0x9b3c94e3, 0x278, 0x4eb0,{ 0xa2, 0xed, 0x5, 0xad, 0xf8, 0xce, 0xa3, 0x9d } };
-	const bool default_write_label_info = true;
+	constexpr GUID guid_write_label_info = { 0x9b3c94e3, 0x278, 0x4eb0,{ 0xa2, 0xed, 0x5, 0xad, 0xf8, 0xce, 0xa3, 0x9d } };
+	constexpr bool default_write_label_info = true;
 	cfg_bool write_label_info(guid_write_label_info, default_write_label_info);
 
-	const GUID guid_write_country = { 0xa983fbd6, 0x7471, 0x41d7,{ 0xa7, 0x62, 0x27, 0xf9, 0x94, 0xe9, 0x23, 0x1f } };
-	const bool default_write_country = true;
+	constexpr GUID guid_write_country = { 0xa983fbd6, 0x7471, 0x41d7,{ 0xa7, 0x62, 0x27, 0xf9, 0x94, 0xe9, 0x23, 0x1f } };
+	constexpr bool default_write_country = true;
 	cfg_bool write_country(guid_write_country, default_write_country);
 
-	const GUID guid_write_format = { 0x8e0f77e2, 0x50e8, 0x4e66,{ 0x88, 0xe3, 0xab, 0xed, 0x3b, 0x76, 0x84, 0x5a } };
-	const bool default_write_format = true;
+	constexpr GUID guid_write_format = { 0x8e0f77e2, 0x50e8, 0x4e66,{ 0x88, 0xe3, 0xab, 0xed, 0x3b, 0x76, 0x84, 0x5a } };
+	constexpr bool default_write_format = true;
 	cfg_bool write_format(guid_write_format, default_write_format);
 
-	const GUID guid_write_asin = { 0x6677d957, 0xaa52, 0x4fef, { 0x95, 0x81, 0xb3, 0xe3, 0x20, 0x16, 0xaf, 0x4d } };
-	const bool default_write_asin = true;
+	constexpr GUID guid_write_asin = { 0x6677d957, 0xaa52, 0x4fef, { 0x95, 0x81, 0xb3, 0xe3, 0x20, 0x16, 0xaf, 0x4d } };
+	constexpr bool default_write_asin = true;
 	cfg_bool write_asin(guid_write_asin, default_write_asin);
 
-	const GUID guid_write_isrc = { 0x8b129571, 0x1bf2, 0x41bd, { 0x86, 0xf1, 0xcd, 0xec, 0x6, 0xb9, 0xf3, 0xff } };
-	const bool default_write_isrc = true;
+	constexpr GUID guid_write_isrc = { 0x8b129571, 0x1bf2, 0x41bd, { 0x86, 0xf1, 0xcd, 0xec, 0x6, 0xb9, 0xf3, 0xff } };
+	constexpr bool default_write_isrc = true;
 	cfg_bool write_isrc(guid_write_isrc, default_write_isrc);
 
-	const GUID guid_write_albumartist = { 0x5cbeb3ba, 0xae9f, 0x4975, { 0xb9, 0x94, 0xd7, 0x50, 0xda, 0x2b, 0x22, 0x0 } };
+	constexpr GUID guid_write_albumartist = { 0x5cbeb3ba, 0xae9f, 0x4975, { 0xb9, 0x94, 0xd7, 0x50, 0xda, 0x2b, 0x22, 0x0 } };
 	const bool default_write_albumartist = false;
 	cfg_bool write_albumartist(guid_write_albumartist, default_write_albumartist);
 
-	const GUID guid_server_data = { 0x8732cde8, 0xe1ed, 0x4824,{ 0xae, 0xf6, 0x7d, 0x4b, 0x56, 0x99, 0x23, 0xac } };
+	constexpr GUID guid_server_data = { 0x8732cde8, 0xe1ed, 0x4824,{ 0xae, 0xf6, 0x7d, 0x4b, 0x56, 0x99, 0x23, 0xac } };
 	cfg_string server_data(guid_server_data, "https://musicbrainz.org");
 
-	const GUID guid_albumtype_data = { 0x20968c09, 0xb0d4, 0x4059,{ 0xb8, 0x92, 0xda, 0x76, 0xf8, 0xe6, 0x51, 0x4e } };
+	constexpr GUID guid_albumtype_data = { 0x20968c09, 0xb0d4, 0x4059,{ 0xb8, 0x92, 0xda, 0x76, 0xf8, 0xe6, 0x51, 0x4e } };
 	cfg_string albumtype_data(guid_albumtype_data, "MUSICBRAINZ_ALBUMTYPE");
 
-	const GUID guid_albumstatus_data = { 0x77182aac, 0x1caa, 0x4793,{ 0xb7, 0x15, 0xcc, 0xf8, 0x97, 0xba, 0x11, 0x1a } };
+	constexpr GUID guid_albumstatus_data = { 0x77182aac, 0x1caa, 0x4793,{ 0xb7, 0x15, 0xcc, 0xf8, 0x97, 0xba, 0x11, 0x1a } };
 	cfg_string albumstatus_data(guid_albumstatus_data, "MUSICBRAINZ_ALBUMSTATUS");
 }
 
@@ -92,22 +92,22 @@ public:
 		albumtype_data = GetDlgItem(IDC_ALBUMTYPE_DATA);
 		albumstatus_data = GetDlgItem(IDC_ALBUMSTATUS_DATA);
 		
-		server_checkbox.SetCheck(mb_preferences::server);
-		short_date_checkbox.SetCheck(mb_preferences::short_date);
-		ascii_punctuation_checkbox.SetCheck(mb_preferences::ascii_punctuation);
-		write_ids_checkbox.SetCheck(mb_preferences::write_ids);
-		write_albumtype_checkbox.SetCheck(mb_preferences::albumtype);
-		write_albumstatus_checkbox.SetCheck(mb_preferences::albumstatus);
-		write_label_info_checkbox.SetCheck(mb_preferences::write_label_info);
-		write_country_checkbox.SetCheck(mb_preferences::write_country);
-		write_format_checkbox.SetCheck(mb_preferences::write_format);
-		write_asin_checkbox.SetCheck(mb_preferences::write_asin);
-		write_isrc_checkbox.SetCheck(mb_preferences::write_isrc);
-		write_albumartist_checkbox.SetCheck(mb_preferences::write_albumartist);
+		server_checkbox.SetCheck(mb_preferences::server.get_value());
+		short_date_checkbox.SetCheck(mb_preferences::short_date.get_value());
+		ascii_punctuation_checkbox.SetCheck(mb_preferences::ascii_punctuation.get_value());
+		write_ids_checkbox.SetCheck(mb_preferences::write_ids.get_value());
+		write_albumtype_checkbox.SetCheck(mb_preferences::albumtype.get_value());
+		write_albumstatus_checkbox.SetCheck(mb_preferences::albumstatus.get_value());
+		write_label_info_checkbox.SetCheck(mb_preferences::write_label_info.get_value());
+		write_country_checkbox.SetCheck(mb_preferences::write_country.get_value());
+		write_format_checkbox.SetCheck(mb_preferences::write_format.get_value());
+		write_asin_checkbox.SetCheck(mb_preferences::write_asin.get_value());
+		write_isrc_checkbox.SetCheck(mb_preferences::write_isrc.get_value());
+		write_albumartist_checkbox.SetCheck(mb_preferences::write_albumartist.get_value());
 
-		server_data.EnableWindow(mb_preferences::server);
-		albumtype_data.EnableWindow(mb_preferences::albumtype);
-		albumstatus_data.EnableWindow(mb_preferences::albumstatus);
+		server_data.EnableWindow(mb_preferences::server.get_value());
+		albumtype_data.EnableWindow(mb_preferences::albumtype.get_value());
+		albumstatus_data.EnableWindow(mb_preferences::albumstatus.get_value());
 
 		uSetWindowText(server_data, mb_preferences::server_data);
 		uSetWindowText(albumtype_data, mb_preferences::albumtype_data);
@@ -118,18 +118,18 @@ public:
 
 	bool has_changed()
 	{
-		if (server_checkbox.IsChecked() != mb_preferences::server) return true;
-		if (short_date_checkbox.IsChecked() != mb_preferences::short_date) return true;
-		if (ascii_punctuation_checkbox.IsChecked() != mb_preferences::ascii_punctuation) return true;
-		if (write_ids_checkbox.IsChecked() != mb_preferences::write_ids) return true;
-		if (write_albumtype_checkbox.IsChecked() != mb_preferences::albumtype) return true;
-		if (write_albumstatus_checkbox.IsChecked() != mb_preferences::albumstatus) return true;
-		if (write_label_info_checkbox.IsChecked() != mb_preferences::write_label_info) return true;
-		if (write_country_checkbox.IsChecked() != mb_preferences::write_country) return true;
-		if (write_format_checkbox.IsChecked() != mb_preferences::write_format) return true;
-		if (write_asin_checkbox.IsChecked() != mb_preferences::write_asin) return true;
-		if (write_isrc_checkbox.IsChecked() != mb_preferences::write_isrc) return true;
-		if (write_albumartist_checkbox.IsChecked() != mb_preferences::write_albumartist) return true;
+		if (server_checkbox.IsChecked() != mb_preferences::server.get_value()) return true;
+		if (short_date_checkbox.IsChecked() != mb_preferences::short_date.get_value()) return true;
+		if (ascii_punctuation_checkbox.IsChecked() != mb_preferences::ascii_punctuation.get_value()) return true;
+		if (write_ids_checkbox.IsChecked() != mb_preferences::write_ids.get_value()) return true;
+		if (write_albumtype_checkbox.IsChecked() != mb_preferences::albumtype.get_value()) return true;
+		if (write_albumstatus_checkbox.IsChecked() != mb_preferences::albumstatus.get_value()) return true;
+		if (write_label_info_checkbox.IsChecked() != mb_preferences::write_label_info.get_value()) return true;
+		if (write_country_checkbox.IsChecked() != mb_preferences::write_country.get_value()) return true;
+		if (write_format_checkbox.IsChecked() != mb_preferences::write_format.get_value()) return true;
+		if (write_asin_checkbox.IsChecked() != mb_preferences::write_asin.get_value()) return true;
+		if (write_isrc_checkbox.IsChecked() != mb_preferences::write_isrc.get_value()) return true;
+		if (write_albumartist_checkbox.IsChecked() != mb_preferences::write_albumartist.get_value()) return true;
 
 		str8 temp;
 		uGetWindowText(server_data, temp);
@@ -171,6 +171,10 @@ public:
 
 	void on_change()
 	{
+		server_data.EnableWindow(server_checkbox.IsChecked());
+		albumtype_data.EnableWindow(write_albumtype_checkbox.IsChecked());
+		albumstatus_data.EnableWindow(write_albumstatus_checkbox.IsChecked());
+
 		m_callback->on_state_changed();
 	}
 
@@ -202,10 +206,6 @@ public:
 
 	void OnChanged(UINT, int, CWindow)
 	{
-		server_data.EnableWindow(server_checkbox.IsChecked());
-		albumtype_data.EnableWindow(write_albumtype_checkbox.IsChecked());
-		albumstatus_data.EnableWindow(write_albumstatus_checkbox.IsChecked());
-
 		on_change();
 	}
 
