833fcd0c456ab42fce889f0ec590d586fdd5e9f2
marc2k3 <34617027+marc2k3@users.noreply.github.com>
2020-01-01 12:41:52 +0000

    2020

--- src/foo_musicbrainz.h
+++ src/foo_musicbrainz.h
@@ -5,5 +5,5 @@ namespace mb
 	static constexpr const char* component_title = "MusicBrainz Tagger";
 	static constexpr const char* component_dll_name = "foo_musicbrainz.dll";
 	static constexpr const char* component_version = "0.4.1";
-	static constexpr const char* component_info = "Copyright (C) 2009-2012 Dremora\nCopyright (C) 2015-2019 marc2003\n\nBuild: " __TIME__ ", " __DATE__;
+	static constexpr const char* component_info = "Copyright (C) 2009-2012 Dremora\nCopyright (C) 2015-2020 marc2003\n\nBuild: " __TIME__ ", " __DATE__;
 }
