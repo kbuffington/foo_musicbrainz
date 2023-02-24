4588dcd8b836dd96a957d43d5986c88c8f3a1a04
kbuffington@gmail.com <kbuffington@gmail.com>
2023-02-23 23:55:14 -0600

    bump to 0.5.0

--- src/foo_musicbrainz.h
+++ src/foo_musicbrainz.h
@@ -4,6 +4,6 @@ namespace mb
 {
 	static constexpr const char* component_title = "MusicBrainz Tagger";
 	static constexpr const char* component_dll_name = "foo_musicbrainz.dll";
-	static constexpr const char* component_version = "0.5.0 beta 1";
+	static constexpr const char* component_version = "0.5.0";
 	static constexpr const char* component_info = "Copyright (C) 2009-2012 Dremora\nCopyright (C) 2015-2020 marc2003\nCopyright (C) 2020-2023 Mordred\n\nBuild: " __TIME__ ", " __DATE__;
 }
